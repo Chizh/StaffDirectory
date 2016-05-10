d75b8b388bf17ef2266cef9fde10fa8acdc94b7b
Artem Astashkin <ArtemAstashkin@gmail.com>
2016-05-11 00:43:46 +0300

    [*] еуыеы

--- RetailRocket.StaffDirectory.Tests/Controllers/API/StaffDataControllerTest.cs
+++ RetailRocket.StaffDirectory.Tests/Controllers/API/StaffDataControllerTest.cs
@@ -4,12 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+using System.IO;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Configuration;
 
 namespace RetailRocket.StaffDirectory.Tests.Controllers.API
 {
     [TestFixture]
     public class StaffDataControllerTest
     {
+        [TestFixtureSetUp]
+        public void TestSetup()
+        {
+            string connectionString =
+                ConfigurationManager.ConnectionStrings["SQLServer_TEST"].ConnectionString;
+            string createDbScriptPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\DB\01_CreateDB.sql");
+            string createDbScript = File.ReadAllText(createDbScriptPath)
+                .Replace("RRStaffDirectory", "RRStaffDirectory_TEST");
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            Server server = new Server(new ServerConnection(conn));
+
+            server.ConnectionContext.ExecuteNonQuery(createDbScript);
+        }
+
         [Test]
         public void ShouldAddTwoNumbers()
         {
