802bb982a3942aa2ba433ab78d00245cbffc1b05
Artem Astashkin <ArtemAstashkin@gmail.com>
2016-05-15 15:58:10 +0300

    [uncommited changes]

---
+++ RetailRocket.StaffDirectory/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using RetailRocket.StaffDirectory.Data;
+
+namespace RetailRocket.StaffDirectory.Controllers
+{
+    public class DepartmentController : ControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View(new List<Department>(Repository.Departments));
+        }
+
+        public ActionResult DepartmentTable()
+        {
+            return PartialView(new List<Department>(Repository.Departments));
+        }
+    }
+}
