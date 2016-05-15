6405afe06da113c08177b1e8cdea4807a6ef129f
Artem Astashkin <ArtemAstashkin@gmail.com>
2016-05-16 00:21:33 +0300

    [*] unstaged files

---
+++ RetailRocket.StaffDirectory/Entity/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RetailRocket.StaffDirectory.Entity
+{
+    public class Staff
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime? Birthday { get; set; }
+        public int? DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
